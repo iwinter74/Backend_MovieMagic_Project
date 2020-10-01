37a0515af6a257079de73336b34f4ca73d976bd3
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-10-01 10:52:15 +0200

    improvements

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
         .then(res => res.json())
         .then(json => {
             data = json.results;
-            res.status(200).render('index', { movies: data})
+            res.status(200).render('index', { movies: data, activeNav: '/' })
         })
         .catch(err => console.log(err))
 })
@@ -44,7 +44,7 @@ app.get('/details/:id', (req, res) => {
         .then(res => res.json())
         .then(json => {
             data = json;
-            res.status(200).render('details', { details: data })
+            res.status(200).render('details', { details: data, activeNav: '' })
         })
         .catch(err => console.log(err))
 })
@@ -55,26 +55,61 @@ app.get('/details/:id/add', (req, res) => {
         .then(res => res.json())
         .then(json => {
             data = json;
-            newItem = new MovieItem({
-                poster_path: data.poster_path,
-                original_title: data.original_title,
-                overview: data.overview,
-                release_date: data.release_date,
-                genres: data.genres,
-                id: data.id,
-                vote_average: data.vote_average,
-                popularity: data.popularity
+            id = req.params.id
+            console.log(id)
+            console.log(data)
+            MovieItem.find({ id: data.id }).then(movie => {
+                console.log(movie)
+                console.log(data.id)
+                if (movie.length > 0) {
+                    console.log('movie already saved')
+                    res.status(200).redirect('/myShows')
+                } else {
+                    newItem = new MovieItem({
+                        poster_path: data.poster_path,
+                        original_title: data.original_title,
+                        overview: data.overview,
+                        release_date: data.release_date,
+                        genres: data.genres,
+                        id: data.id,
+                        vote_average: data.vote_average,
+                        popularity: data.popularity
+                    })
+                    newItem.save()
+                    console.log('new movie saved')
+                    res.status(200).redirect('/myShows')
+                }
             })
-            newItem.save()
-            res.status(200).redirect('/myShows')
-    })
-    .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
 })
+
+// app.get('/details/:id/add', (req, res) => {
+//     console.log('add ' + req.params.id)
+//     fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.apiKey}&language=en-US`)
+//         .then(res => res.json())
+//         .then(json => {
+//             data = json;
+//             newItem = new MovieItem({
+//                 poster_path: data.poster_path,
+//                 original_title: data.original_title,
+//                 overview: data.overview,
+//                 release_date: data.release_date,
+//                 genres: data.genres,
+//                 id: data.id,
+//                 vote_average: data.vote_average,
+//                 popularity: data.popularity
+//             })
+//             newItem.save()
+//             res.status(200).redirect('/myShows')
+//     })
+//     .catch(err => console.log(err))
+// })
 
 app.get('/myShows', (req, res) => {
     MovieItem.find()
         .then(result => {
-            res.status(200).render('myShows', { movies: result })
+            res.status(200).render('myShows', { movies: result, activeNav: '/myShows' })
         })
         .catch(err => console.log(err))
 })
@@ -82,15 +117,15 @@ app.get('/myShows', (req, res) => {
 app.get('/detailsDb/:id', (req, res) => {
     MovieItem.findById(req.params.id)
         .then(result => {
-            console.log(result.genres)
-            res.status(200).render('detailsDb', { details: result })
+            // console.log(result.genres)
+            res.status(200).render('detailsDb', { details: result, activeNav: '' })
         })
         .catch(err => console.log(err))
 })
 
 app.get('/details/:id/delete', (req, res) => {
     console.log(`delete ` + req.params.id)
-       MovieItem.findByIdAndDelete(req.params.id)
+    MovieItem.findByIdAndDelete(req.params.id)
         .then(result => {
             res.status(200).redirect('/myShows')
         })
@@ -99,13 +134,13 @@ app.get('/details/:id/delete', (req, res) => {
 
 app.post('/search', (req, res) => {
     query = req.body.search
-    console.log(query)
+    // console.log(query)
     fetch(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.apiKey}&language=en-US&query=${query}&page=1&include_adult=false`)
         .then(res => res.json())
         .then(json => {
             data = json.results;
-            console.log(data)
-            res.status(200).render('search', { movies: data})
+            // console.log(data)
+            res.status(200).render('search', { movies: data, activeNav: '' })
         })
         .catch(err => console.log(err))
 })
